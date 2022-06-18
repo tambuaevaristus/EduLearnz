33b2f27b7629763d35e01f4f2e566ad382f5faa0
Tambua Evaristus <evaristustambua@gmail.com>
2022-06-18 22:01:59 +0100

    small work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import react from 'react';
+import react from "react";
 
-import Header from './components/Header';
-import Login from './screen/Login';
+import Header from "./components/Header";
+import Login from "./screen/Login";
 
-import Home from './screen/Home';
+import Home from "./screen/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import CourseContent from "./screen/CourseContent";
 
 function App() {
   return (
-     
-
-      <Router>
+    <Router>
       <div className="app">
         <Switch>
           <Route path="/login">
-            <Login/>
+            <Login />
+          </Route>
+          <Route path="/course/{id}">
+             {/* <Header /> */}
+            <CourseContent />
           </Route>
           <Route path="/">
-          <Header />
+            <Header />
             <Home />
           </Route>
+        
         </Switch>
       </div>
     </Router>
