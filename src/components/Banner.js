454a39b34556dd0121e10091ec8422711cbb7701
Tambua Evaristus <evaristustambua@gmail.com>
2022-06-19 22:37:17 +0100

    latest changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,14 +11,14 @@ export default function Banner() {
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img height="600px" src="https://images.unsplash.com/photo-1629904853716-f0bc54eea481?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" class="d-block w-100" alt="..." />
-        <div class="carousel-caption  d-md-block">
+        <div class="carousel-caption  d-block">
           <h1>Creacting A Pefect Place For Studies</h1>
           <p>Some representative placeholder content for the first slide.</p>
         </div>
       </div>
       <div class="carousel-item">
         <img height="600px" src="https://images.unsplash.com/photo-1619410283995-43d9134e7656?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="d-block w-100" alt="..." />
-        <div class="carousel-caption  d-md-block">
+        <div class="carousel-caption  d-block">
           <h1>All Courses Are Free</h1>
           <p>Some representative placeholder content for the second slide.</p>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -36,7 +36,7 @@ const Header = () => {
   };
 
   return (
-    <nav class="navbar navbar-expand-lg py-3 navbar-light bg-light">
+    <nav class="navbar fixed-top navbar-expand-lg py-3 navbar-light bg-light">
       <div class="container-fluid">
         <a class="navbar-brand font-weight-bold" href="#">EduLEARNZ</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
